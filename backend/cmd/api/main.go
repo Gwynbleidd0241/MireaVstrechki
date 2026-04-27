03ded0c2934d9bcd5414875bad0a23570ad09104
Sergey Lazarenko <sergey.lazarenko.0241@mail.ru>
2026-04-28 00:30:43 +0300

    embed SQL into binary

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"github.com/pressly/goose/v3"
 
 	"meeting-service/internal/config"
 	httpserver "meeting-service/internal/http"
 	"meeting-service/internal/logger"
 	"meeting-service/internal/repository/postgres"
 	"meeting-service/internal/service"
+	"meeting-service/migrations"
 )
 
 func main() {
 	cfg := config.Load()
+
+	if len(os.Args) > 1 && os.Args[1] == "migrate" {
+		runMigrations(cfg.Postgres.DSN)
+		return
+	}
 
 	logg, err := logger.New()
 	if err != nil {
@@ -39,4 +48,24 @@ func main() {
 
 	server := httpserver.New(cfg, logg, userService, eventService, taskService, participantService)
 	server.Run()
+}
+
+func runMigrations(dsn string) {
+	db, err := postgres.New(dsn)
+	if err != nil {
+		log.Fatalf("migrate: connect: %v", err)
+	}
+	defer db.Close()
+
+	goose.SetBaseFS(migrations.FS)
+
+	if err := goose.SetDialect("postgres"); err != nil {
+		log.Fatalf("migrate: dialect: %v", err)
+	}
+
+	if err := goose.Up(db, "."); err != nil {
+		log.Fatalf("migrate: up: %v", err)
+	}
+
+	log.Println("migrations applied")
 }
